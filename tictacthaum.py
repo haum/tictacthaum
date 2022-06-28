fc6729831dab29867da398da28f0ca687141211a
JackDesBwa <jackdesbwa@gmail.com>
2022-06-28 22:33:18 +0200

    Remove licence header in code

--- tictacthaum.py
+++ tictacthaum.py
@@ -1,16 +1,5 @@
 #! /usr/bin/env python3
 # -*- coding:utf8 -*-
-#
-# tictacthaum.py
-#
-# Copyright © 2022 HAUM <contact@haum.org>
-#
-# Licensed under the "THE BEER-WARE LICENSE" (Revision 42):
-# Mathieu (matael) Gaborit wrote this file. As long as you retain this notice
-# you can do whatever you want with this stuff. If we meet some day, and you
-# think this stuff is worth it, you can buy me a beer or coffee in return
-#
-
 
 import time
 
