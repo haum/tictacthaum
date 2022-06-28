8a3e4f0ec792226b4e9a0c3c55541c408823cc66
JackDesBwa <jackdesbwa@gmail.com>
2022-06-29 00:25:07 +0200

    Add initial cube pattern

--- tictacthaum.py
+++ tictacthaum.py
@@ -3,6 +3,7 @@
 
 import time
 
+from utils import coord_3d_to_linear
 from controllers import Cube, BlinkAnimator, StillAnimator
 from legacy import TalController
 
@@ -10,6 +11,10 @@ timestep = 1/30
 
 tc = TalController()
 c = Cube(tc)
+
+c.set_animator(coord_3d_to_linear(3, 0, 0), StillAnimator((255, 0, 0)))
+c.set_animator(coord_3d_to_linear(0, 3, 0), StillAnimator((0, 255, 0)))
+c.set_animator(coord_3d_to_linear(0, 0, 3), StillAnimator((0, 0, 255)))
 
 while True:
     time.sleep(timestep)
