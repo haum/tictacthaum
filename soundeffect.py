2f8c835d30047ff90f11d8cd1088bee970c27ba6
JbLb <jerome@jblb.net>
2022-10-05 23:47:44 +0200

    new lib for playing sounds

--- soundeffect.py
+++ soundeffect.py
@@ -1,22 +1,25 @@
+sound_lib = True
 try:
-    import pyglet
+    from pydub import AudioSegment
 except ModuleNotFoundError:
-    pyglet = False
+    sound_lib = False
+try:
+    from pydub.playback import play
+except ModuleNotFoundError:
+    sound_lib = False
 
 class SoundEffect:
     def __init__(self, disable = False):
-        if not pyglet or disable:
+        if not sound_lib or disable:
             self.sounds = {}
-            print('Sound inactive:', 'disabled' if disable else 'pyglet not found')
+            print('Sound inactive:', 'disabled' if disable else 'sound lib not found')
             return
         else:
             print('Sound active')
-        pyglet.resource.path = ['./']
-        pyglet.resource.reindex()
         self.sounds = {
-            'ping': pyglet.resource.media('ping.ogg', streaming=False),
-            'ping2': pyglet.resource.media('ping2.ogg', streaming=False),
-            'end': pyglet.resource.media('end.ogg', streaming=False),
+            'ping': AudioSegment.from_ogg('ping.ogg'),
+            'ping2': AudioSegment.from_ogg('ping2.ogg'),
+            'end': AudioSegment.from_ogg('end.ogg'),
         }
 
     def play(self, n):
